f71530e787177684656b5358fafa434eda1eee64
chungjung-d <chungjung.d@gmail.com>
2025-05-14 21:48:49 +0900

    darkmode disable:

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html
       lang="ko"
-      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} antialiased light`}
     >
       <body>
         <Providers>
